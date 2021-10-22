3e2fc3154f68f461a1eb0ccfcabfff49922a73a0
ThatCoderKid726 <83380065+ThatCoderKid726@users.noreply.github.com>
2021-10-22 17:24:03 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -23,12 +23,18 @@ var Content = event.results[0][0].transcript;
 function speak() {
 var synth = window.speechSynthesis;
 
-speak_data = document.getElementById ("textbox").value;
+speak_data = "Taking your Selfie in 5 seconds";
 
 var utterThis = new SpeechSynthesisUtterance (speak_data);
 
 synth.speak(utterThis);
 Webcam.attach(camera);
+
+setTimeout(function(){
+    take_snapshot();
+    save();
+
+},5000);
 }
 
 Webcam.set({
@@ -38,3 +44,17 @@ Webcam.set({
     png_quality:90
 });
 camera=document.getElementById ("camera");
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+function save()
+{
+  link = document.getElementById("link");
+  image = document.getElementById("selfie_image").src ;
+  link.href = image;
+  link.click();
+}
